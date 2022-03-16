166aa2499eb1b3484db0452ac9d78a2cd82bf620
unknown <SankaranarayananS48>
2022-03-16 11:40:38 +0530

    Entity framework -DBFirstApproach

---
+++ Day16/DBFirstApproach/LinqCRUD/LinqCRUD/StudentRegForm.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="StudentRegForm.aspx.cs" Inherits="LinqCRUD.StudentRegForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+       <center><h2>Student Registration CRUD Operation</h2></center> 
+        <br /><br />
+        <fieldset>
+            <legend>Student Information</legend>
+            <h3>Id     <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            </h3>
+            <h3>Name    <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            </h3>
+            <h3>Phoneno  <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+            </h3>
+        
+        <br />
+        <asp:Button ID="Button1" runat="server" Text="Insert" OnClick="Button1_Click" />
+        <asp:Button ID="Button2" runat="server" Text="Update" OnClick="Button2_Click" />
+        <asp:Button ID="Button3" runat="server" Text="Delete" OnClick="Button3_Click" />
+        <asp:Button ID="Button4" runat="server" Text="Display" OnClick="Button4_Click" />
+        <br /><br />
+        </fieldset>
+        <asp:GridView ID="GridView1" runat="server" BackColor="Black" BorderColor="#000066" BorderStyle="Dashed" ForeColor="White">
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ Day16/DBFirstApproach/LinqCRUD/LinqCRUD/StudentRegForm.aspx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LinqCRUD
+{
+    public partial class StudentRegForm : System.Web.UI.Page
+    {
+        studentEntities1 db = new studentEntities1();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            studentreg table = new studentreg();
+            table.rollno = TextBox1.Text;
+            table.name = TextBox2.Text;
+            table.phoneno = TextBox3.Text;
+            db.studentregs.Add(table);
+            db.SaveChanges();
+            Display();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            studentreg table1 = db.studentregs.FirstOrDefault(record=>record.rollno==TextBox1.Text);
+            table1.name = TextBox2.Text;
+            table1.phoneno = TextBox3.Text;
+            db.SaveChanges();
+            Display();
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            studentreg table2 = db.studentregs.FirstOrDefault(record => record.rollno == TextBox1.Text);
+            db.studentregs.Remove(table2);
+            db.SaveChanges();
+            Display();
+        }
+        public void Display()
+        {
+            GridView1.DataSource = db.studentregs.ToArray();
+            GridView1.DataBind();
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            Display();
+        }
+    }
+}
