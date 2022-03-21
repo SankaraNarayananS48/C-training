ea5a77c2146dbb2cf2d84533ee190fa209a42f29
Sankara Narayanan S <SankaranarayananS48>
2022-03-21 16:27:16 +0530

    ActionFilter

---
+++ Day19/ActionFilterExample/ActionFilterExample/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ActionFilterExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+        
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        [OutputCache(Duration =10)]
+        public ActionResult Index(FormCollection Collection)
+        {
+            ViewBag.Username = Collection["name"];
+            return View();
+        }
+        [HandleError]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            int a = 10, b = 0;
+            int c = a / b;
+            RedirectToAction("/Home/About/c");
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
